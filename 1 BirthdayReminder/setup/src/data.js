55e30e3e2624f21a6e6869ce74565858024b661d
Guptaverse <sggreat88@gmail.com>
2022-05-02 06:41:30 +0530

    Birthday reminder added

---
+++ 1 BirthdayReminder/setup/src/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 1,
+    name: 'Abhishek Vats',
+    age: 21,
+    image:
+      // 'https://drive.google.com/file/d/11xqgxFeXmqd9mCPIyxVu1R2hER88lYqR/uc?usp=sharing'
+      'https://drive.google.com/uc?export=view&id=11xqgxFeXmqd9mCPIyxVu1R2hER88lYqR',
+  },
+  {
+    id: 2,
+    name: 'Bhanu Arora',
+    age: 20,
+    image:
+    // https://drive.google.com/file/d/10fhFkhuPjv6Pj6uhJccLDQ7uCEob_7UV/view?usp=sharing
+    'https://drive.google.com/uc?export=view&id=10fhFkhuPjv6Pj6uhJccLDQ7uCEob_7UV',
+  },
+  {
+    id: 3,
+    name: 'Pankaj',
+    age: 22,
+    image:
+    'https://drive.google.com/uc?export=view&id=140kVFgh0q5RUbCiZowi_gJ8p_BmtLpPb',
+
+  },
+  {
+    id: 4,
+    name: 'Sumit Shakya',
+    age: 22,
+    image:
+    'https://drive.google.com/uc?export=view&id=1cF27aRZHQ4m7ZFYglL7_0bgCZutIGlhk',
+    
+  },
+  {
+    id: 5,
+    name: 'Sukhvinder Satija',
+    age: 21,
+    image:
+    'https://drive.google.com/uc?export=view&id=1bvuUvtQA3Xxaua0T4lnOaeE7lMSkuREm'
+    ,
+  },
+];
